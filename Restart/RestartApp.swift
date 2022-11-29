c3507ec97a3ea7002dd755ff3a119902a4c38c56
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-11-29 18:25:21 -0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by kenjimaeda on 29/11/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
